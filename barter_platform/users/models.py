ed9f064f7d353c0c71e423d8174d0aa23d110f94
6igsm0ke <kosbayevadilet@gmail.com>
2025-05-17 19:59:29 +0600

    Errors fixed and gitignore added

--- barter_platform/users/models.py
+++ barter_platform/users/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from .managers import UserManager
+from django.utils import timezone
 
 # Create your models here.
 class User(AbstractBaseUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- barter_platform/users/serializers.py
+++ barter_platform/users/serializers.py
@@ -9,8 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "email",
+            "password",
         ]
-        read_only_fields = ["id"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
